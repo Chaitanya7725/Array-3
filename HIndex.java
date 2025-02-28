b361c7f9779483007b93b08dff6af21cc92c3d49
Chaitanya7725 <chaitanya7725@gmail.com>
2025-02-27 22:35:44 -0500

    Added multiple approaches

--- HIndex.java
+++ HIndex.java
@@ -1,10 +1,39 @@
+
+// Using Sorting and Linear Search
+// TC: O(n log n)
+// SC: O(1)
+
+// Using Bucket Sort
+// TC: O(n)
+// SC: O(n)
+
+import java.util.Arrays;
+
 public class HIndex {
     public static void main(String[] args) {
-        System.out.println(hIndex(new int[] { 3, 0, 6, 1, 5 })); // 3
-        System.out.println(hIndex(new int[] { 1, 3, 1 })); // 1
+        System.out.println(hIndexLinear(new int[] { 3, 0, 6, 1, 5 })); // 3
+        System.out.println(hIndexLinear(new int[] { 1, 3, 1 })); // 1
+
+        System.out.println(hIndexBucketSort(new int[] { 3, 0, 6, 1, 5 })); // 3
+        System.out.println(hIndexBucketSort(new int[] { 1, 3, 1 })); // 1
     }
 
-    public static int hIndex(int[] citations) {
+    // Sort first and then traverse from end
+    public static int hIndexLinear(int[] citations) {
+        if (citations == null || citations.length == 0)
+            return 0;
+        Arrays.sort(citations);
+        int n = citations.length;
+        int i = 0;
+        while (i < n && citations[n - i - 1] > i) {
+            i++;
+        }
+        return i;
+    }
+
+    // Sorting array in buckets which contributes to O(n) TC.
+    // Extra array is used for storing the citations indexes value.
+    public static int hIndexBucketSort(int[] citations) {
         if (citations == null || citations.length == 0)
             return 0;
         int n = citations.length;
